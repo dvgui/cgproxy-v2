a8cf4bd24bbee315c47481a6141dc482668846f7
dvgui <guidito.ito@gmail.com>
2023-05-09 15:17:25 +0200

    handler

--- init.ts
+++ init.ts
@@ -8,12 +8,15 @@ const port = process.env.PORT || 3000;
 
 const proxy = 'http://51.159.115.233:3128'; // replace with your proxy server
 
-app.use('/', createProxyMiddleware({
+const handler = createProxyMiddleware({
   target: 'https://api.coingecko.com',
   changeOrigin: true,
   router: {
     'api.coingecko.com': proxy
   }
-}));
+});
+
+app.use('/', handler);
+
 
 app.listen(port);
